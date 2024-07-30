c52ec173e9196e8252a8aad92bca44e64cad1674
Ivan Stankovic <istankovic@hotmail.com>
2024-07-30 22:30:56 +0200

    Add project files.

---
+++ MudRoles/Controllers/ApikeysController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MudRoles.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApikeysController : ControllerBase
+    {
+        [Authorize]
+        [HttpGet]
+        public async Task<IActionResult> GetItem()
+        {
+            await Task.Yield();
+            var response = new { message = "Lorem ipsum dolor sit amet, consectetur adipiscing elit." };
+
+            // Your logic to add an item
+            return Ok(response);
+        }
+    }
+}
